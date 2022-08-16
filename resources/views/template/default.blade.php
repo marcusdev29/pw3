c59cebebfd37eddb10ed5cb03141baf9113f094d
Marcus <107251339+marcusprodev@users.noreply.github.com>
2022-08-16 11:58:01 -0300

    Update default.blade.php

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -7,28 +7,61 @@
     <title>Site de cria</title>
 
     <link rel="stylesheet" href="{{url('css/style.css')}}">
+     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    
 
 </head>
 <body>
     <header>
-        <nav>
-        <a href="/" > Home </a>
-    <a href="/contato" > Contato </a>
-    <a href="/cliente" > Cliente </a>
-    <a href="/pedido" > Pedido </a>
-    <a href="/produto" > Produto </a>
-    <a href="/categoria" > Categoria </a>
-        </nav>
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link" href="/contato">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/pedido">Pedido</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/produto">Produto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/categoria">Categoria</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </header>
 
 
     @yield('content')
 
     <footer>
-    <p>Todos direitos reservados mini d - 2022</p>
-<img class="chave" src="{{url('images/chavedms.jpg')}}">
+    
+        <img class="chave1" src="{{url('images/chavedms.jpg')}}">
+        <img src="{{url('images/chavedms.jpg')}}" class="img-fluid" alt="...">
 
-</footer>
+        <div class="card">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>Todos direitos reservados Marcus, Diego e Diegão - 2022
+      </p>
+      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+    </blockquote>
+  </div>
+</div>
+
+    </footer>
 
 </body>
 
